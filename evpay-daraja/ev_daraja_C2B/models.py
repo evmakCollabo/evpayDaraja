4ecdff42a97e0425404d032fe2c6ee0ad6247cf2
NjeriMaina4172 <lucymaina4172@gmail.com>
2023-07-19 21:11:40 +0300

    modifying the user class

--- evpay-daraja/ev_daraja_C2B/models.py
+++ evpay-daraja/ev_daraja_C2B/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 
 class User(models.Model):
+    INDIVIDUAL = 'IND'
+    BUSINESS = 'BUS'
+    BUSINESS_TYPES = [
+        (INDIVIDUAL, 'Individual'),
+        (BUSINESS, 'Business/Organization'),
+    ]
+
     email = models.CharField(max_length=128, null=False)
     password = models.CharField(max_length=128, null=False)
-    first_name = models.CharField(max_length=128, null=True)
-    last_name = models.CharField(max_length=128, null=True)
+    name = models.CharField(max_length=128, null=True)
     mobile_number = models.CharField(max_length=20, null=True, unique=True)
+    business_type = models.CharField(max_length=3, choices=BUSINESS_TYPES, default=INDIVIDUAL)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.mobile_number} - {self.email}"
+        return f"{self.name} - {self.mobile_number} - {self.email}"
 
 class TransactionLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    datetime = models.CharField(max_length=20)
+    datetime = models.DateTimeField()
     trans_ref = models.CharField(max_length=20, primary_key=True)
     amount = models.IntegerField()
     source_from = models.CharField(max_length=50)
